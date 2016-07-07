64a76e33acd0a2d623673ef5e11b264398bda9bf
Keith Armstrong <keith1015@gmail.com>
2016-07-07 02:40:26 -0500

    improve directory listing

--- utils.js
+++ utils.js
@@ -1,6 +1,8 @@
 const VERSION = "0.2";
 const Promise = require('bluebird');
+const _       = require('lodash');
 const path    = require('path');
+const async    = require('async');
 const fs      = Promise.promisifyAll(require("fs"));
 
 let options;
@@ -41,27 +43,43 @@ module.exports = {
   },
   genDirectoryListing(dir, request) {
     return new Promise((resolve, reject) => {
+      let files = [];
+      let dirs = [];
       let body = "";
-      fs.readdirAsync(dir).then(files => {
-        body = `<!DOCTYPE html>
-          <html lang="en">
-           <head>
-            <title>Index of ${request}</title>
-           </head>
-           <body>
-          <h1>Index of ${request}</h1>
-          <ul>
-          <li><a href="${path.dirname(request)}">..</a></li>
-        `;
-        files.forEach(file => {
-          body += `<li><a href="${path.join(request, file)}">${file}</a></li>`;
+      fs.readdirAsync(dir).then(items => {
+        async.each(items, (item, cb) => {
+          isDir(path.join(options['static'], path.join(request, item))).then(dir => {
+            if (dir) {
+              dirs.push({name: item, html: `<a href="${path.join(request, item)}">${item}/</a>`});
+            } else {
+              files.push({name: item, html: `<a href="${path.join(request, item)}">${item}</a>`});
+            }
+            cb();
+          });
+        }, () => {
+          dirs = _.sortBy(dirs, 'name');
+          files = _.sortBy(files, 'name');
+          items = _.concat(dirs, files);
+          body = `<!DOCTYPE html>
+            <html lang="en">
+             <head>
+              <title>Index of ${request}</title>
+             </head>
+             <body>
+            <h1>Index of ${request}</h1>
+            <ul>
+            <li><a href="${path.dirname(request)}">..</a></li>
+          `;
+          _.each(items, item => {
+            body += `<li>${item.html}</li>`;
+          });
+          body += `
+            </ul>
+            <address>Lobabob Server v${VERSION} Port ${options['port']}</address>
+            </body></html>
+          `;
+          resolve(body);
         });
-        body += `
-          </ul>
-          <address>Lobabob Server v${VERSION} Port ${options['port']}</address>
-          </body></html>
-        `;
-        resolve(body);
       }, err => {
         reject(body);
       });
@@ -86,3 +104,11 @@ module.exports = {
   },
   VERSION
 };
+
+function isDir(path) {
+  return new Promise((resolve, reject) => {
+    fs.statAsync(path).then(stat => {
+      resolve(stat.isDirectory());
+    });
+  });
+}
